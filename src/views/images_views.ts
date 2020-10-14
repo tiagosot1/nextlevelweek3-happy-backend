e46711d94e73c0bc9d100d3ebf2adda991a7b95f
Tiago Oliveira Souza <tiagocdc2008@gmail.com>
2020-10-14 15:24:50 -0300

    alteraçao de variavel dos orphanages

--- src/views/images_views.ts
+++ src/views/images_views.ts
@@ -2,7 +2,7 @@ import Image from '../models/Image';
 export default {
     render(image: Image) {
         return {
-            image: image.id, 
+            id: image.id, 
             url:`http://localhost:3333/uploads/${image.path}`
         };
     },
